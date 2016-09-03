8160f6388c1494d50e0371458e5f0af0bfb84c13
Adrian L Lange <mail@p3lim.net>
2016-09-03 18:03:21 +0200

    :sparkles:

---
+++ README.md
@@ -0,0 +1,26 @@
+# oUF Resurrect
+
+This is an addon for the popular MMORPG "World of Warcraft".  
+It can be downloaded at [Curse](//mods.curse.com/addons/wow/ouf-resurrect) or at [WoWInterface](//wowinterface.com/downloads/info00000).
+
+## Description
+
+oUF Resurrect is a element plug-in made for the unitframe framework oUF.
+It does nothing by itself, and requires a layout that supports it to function.
+
+When a player is being resurrected, by (almost) any means, it will display an
+icon on the unit being resurrected.
+
+Powered by [LibResInfo](//github.com/Phanx/LibResInfo), this plugin has exceptional accuracy.
+
+For details on how to implement this in your own layout, please see the wiki:  
+<https://github.com/p3lim-wow/oUF_Resurrect/wiki>
+
+## Feedback
+
+If you have a question, please use the comments section on Curse/WoWInterface.  
+If you would like to report a bug or contribute to the project, please follow [this link](//github.com/p3lim-wow/oUF_Resurrect/issues?q=) to get started.
+
+## Legal
+
+Please see the [LICENSE](//github.com/p3lim-wow/oUF_Resurrect/blob/master/LICENSE.txt) file.
---
+++ oUF_Resurrect.lua
@@ -0,0 +1,98 @@
+local _, ns = ...
+local oUF = ns.oUF or oUF
+local elementName = 'oUF Resurrect'
+assert(oUF, elementName .. ' was unable to locate oUF install')
+
+local LRI = LibStub('LibResInfo-1.0', true)
+assert(LRI, elementName .. ' requires LibResInfo-1.0')
+
+oUF.colors.resurrect = {
+	CASTING = {1, 1, 1}, -- XXX
+	MASSRES = {1, 1, 1}, -- XXX
+	SELFRES = {1, 1, 1}, -- XXX
+	PENDING = {1, 1, 1}, -- XXX
+}
+
+local function Update(self, event, unit)
+	if(unit ~= self.unit) then
+		return
+	end
+
+	local element = self.Resurrect
+	if(element.PreUpdate) then
+		element:PreUpdate()
+	end
+
+	local status, endTime, casterUnit = LRI:UnitHasIncomingRes(unit)
+	if(status) then
+		local colors = self.colors.resurrect[status]
+		element:SetVertexColor(colors[1], colors[2], colors[3])
+		element:Show()
+	else
+		element:Hide()
+	end
+
+	if(element.PostUpdate) then
+		return element:PostUpdate(status, endTime, casterUnit)
+	end
+end
+
+local function Path(self, ...)
+	return (self.Resurrect.Override or Update)(self, ...)
+end
+
+local function ForceUpdate(element)
+	local parent = element.__owner
+	return Path(parent, 'ForceUpdate', parent.unit)
+end
+
+local REGISTERED = {}
+local function RegisterCallbacks(element)
+	table.insert(REGISTERED, element)
+
+	if(#REGISTERED == 1) then
+		LRI.RegisterAllCallbacks(elementName, function(event)
+			for index = 1, #oUF.objects do
+				local frame = oUF.objects[index]
+				if(frame.unit and frame.Resurrect) then
+					Path(frame, event, frame.unit)
+				end
+			end
+		end, true)
+	end
+end
+
+local function UnregisterCallbacks(element)
+	table.remove(REGISTERED, element)
+
+	if(#REGISTERED == 0) then
+		LRI.UnregisterAllCallbacks(elementName)
+	end
+end
+
+local function Enable(self)
+	local element = self.Resurrect
+	if(element) then
+		element.__owner = self
+		element.ForceUpdate = ForceUpdate
+
+		RegisterCallbacks(element)
+
+		if(element:IsObjectType('Texture') and not element:GetTexture()) then
+			element:SetTexture([[Interface\RaidFrame\Raid-Icon-Rez]])
+		end
+
+		return true
+	end
+end
+
+local function Disable(self)
+	local element = self.Resurrect
+	if(element) then
+		element:Hide()
+
+		UnregisterCallbacks(element)
+	end
+end
+
+oUF:AddElement('Resurrect', Path, Enable, Disable)
